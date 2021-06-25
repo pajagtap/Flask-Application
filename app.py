158b3d7836eaeba4070504210ab45bbb6aeb319b
Pratik Jagtap <30865275+prat118@users.noreply.github.com>
2021-06-26 00:26:58 +0530

    Created and added app.py file
    This is basically a file where our flask app will be created and run. This is basic application. GetData is another module or python file which will be used to read the data from csv file.

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+# custom modules
+import GetData
+
+# set the required parameters for rest app
+app = Flask(__name__)
+
+# Books class as a resource to be used for rest operations
+class Book(Resource):
+	def get(self):
+		request_data = request.get_json()
+		if ("rows" in request_data.keys()) and (type(request_data["rows"])==int):
+			books = GetData.getRows("/books-2.csv",request_data["rows"])
+			return {"books":books}
+		else:
+			data = GetData.getFilteredData('././books-2.csv',request_data)
+			if type(data)==str:
+				return {"message":data}
+			else:
+				return {"books":data}
+
+if __name__=="__main__":
+	# add the resource to the api and run the app
+	api.add_resource(Book, "/books")
+	app.run(host="0.0.0.0",port=5000, debug=True)
