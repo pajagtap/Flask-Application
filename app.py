832306cd1b25795f6d16fc2423ea69d7400374e6
Pratik Jagtap <30865275+prat118@users.noreply.github.com>
2021-06-26 17:15:38 +0530

    Authentication implementation and response modification
    Added authentication using JSON web tokens for accessing the rest api. Currently its kept as commented. Made some changes in the response codes.

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
 
-# custom modules
+# custom imports
+from userverify import verify
 import GetData
 
 # set the required parameters for rest app
 app = Flask(__name__)
+app.secret_key = "1!qQ2@wW"
+api = Api(app)
+
+# authentication using JWT
+jwt = JWT(app, verify)
+
 
 # Books class as a resource to be used for rest operations
 class Book(Resource):
+	# @jwt_required()
 	def get(self):
 		request_data = request.get_json()
 		if ("rows" in request_data.keys()) and (type(request_data["rows"])==int):
-			books = GetData.getRows("/books-2.csv",request_data["rows"])
+			books = GetData.getRows("books-2.csv",request_data["rows"])
 			return {"books":books}
 		else:
 			data = GetData.getFilteredData('././books-2.csv',request_data)
 			if type(data)==str:
-				return {"message":data},404	# 404-Not Found response when books are not found
+				return {"message":data}, 404
 			else:
-				return {"books":data},200	# 200 Successful response
+				return {"books":data}, 200
 
 if __name__=="__main__":
 	# add the resource to the api and run the app
